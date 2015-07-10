e6195a7c3abb1ec3874f377fc35ba484a5e0cdf6
Vasily Khodyrev <vasily.khodyrev@alcatel-lucent.com>
2015-07-10 15:42:41 +0300

    Some peace of code added for Schema #2

--- src/main/java/com/alu/tat/init/Init.java
+++ src/main/java/com/alu/tat/init/Init.java
@@ -6,6 +6,10 @@ import javax.servlet.http.HttpServlet;
 import com.alu.tat.entity.Task;
 import com.alu.tat.entity.User;
 import com.alu.tat.entity.dao.BaseDao;
+import com.alu.tat.entity.schema.Schema;
+import com.alu.tat.entity.schema.SchemaElement;
+
+import java.util.List;
 
 /**
  * Created by imalolet on 6/19/2015.
@@ -35,5 +39,18 @@ public class Init extends HttpServlet {
 
             BaseDao.create(t);
         }
+
+        Schema schema = new Schema();
+        schema.setName("Default schema");
+        schema.setDescription("Default schema");
+        SchemaElement sdd = new SchemaElement("SDD","Do we need SDD?",SchemaElement.ElemType.BOOLEAN,5);
+        SchemaElement impl = new SchemaElement("impl","Do we need to do it?",SchemaElement.ElemType.BOOLEAN,5);
+        SchemaElement test = new SchemaElement("test","How many tests?",SchemaElement.ElemType.INTEGER,5);
+        List<SchemaElement> list  = schema.getElementsList();
+        list.add(sdd);
+        list.add(impl);
+        list.add(test);
+
+        BaseDao.create(schema);
     }
 }
